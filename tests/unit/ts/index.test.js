b381f31da8ac24b0c8d217122e98a82e2aa75bd3
Cognify-source <fredrik.ekholm@protonmail.com>
2025-08-03 14:50:38 +0000

    rens js

--- tests/unit/ts/index.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// tests/unit/ts/index.test.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../../../src/ts/index");
-describe("orchestrator main", () => {
-    it("ska exportera en main-funktion", () => {
-        expect(typeof index_1.main).toBe("function");
-    });
-});
